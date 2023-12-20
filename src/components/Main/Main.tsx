2cc0b349e84268c2b1d759c5ee70336b3d5bc7ac
RaidEyes <phamquanghung199713@gmail.com>
2023-12-20 21:55:49 +0700

    Initial main design

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,27 @@
+import data from "../../data/data.json";
+
+interface dataObject {
+  day: string;
+  amount: number;
+}
+
 function Main() {
+  const dates: dataObject[] = data;
+  console.log(dates);
+
   return (
-    <div>
+    <div className="bg-white rounded-xl p-4">
       <h2>Spending - Last 7 days</h2>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+      <ul className="py-4 flex flex-row gap-2 justify-between items-end">
+        {dates.map((date: { day: string; amount: number }) => (
+          <li key={date.day} className="flex flex-col gap-2 items-center">
+            <div
+              className="bg-[var(--primary-red)] w-7 rounded-[0.2rem]"
+              style={{ height: `${date.amount * 3}px` }}
+            ></div>
+            <p className="text-xs">{date.day}</p>
+          </li>
+        ))}
       </ul>
       <div>
         <p>Total this month</p>
